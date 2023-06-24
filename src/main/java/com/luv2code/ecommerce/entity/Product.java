cd70bf45416085ed27f62cc190a25b8da9145e2e
manTTale <mantale.roberto1@yahoo.com>
2023-06-24 18:04:04 +0300

    configured the Product entity

---
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -0,0 +1,30 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active;
+    private int unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdated;
+
+
+
+
+
+}
