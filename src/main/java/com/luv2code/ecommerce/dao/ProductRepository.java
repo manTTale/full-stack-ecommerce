1e3e5874203ba7a45b7d21f2e302dbc995b0b81e
manTTale <mantale.roberto1@yahoo.com>
2023-06-25 21:25:05 +0300

    configured CORS

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//we can accept calls from web browser scripts (our frontend) for this origin
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
